1ae56930f01ceb73bb1820378b70a4a3eb861217
Til Birnstiel <birnstiel@me.com>
2023-08-31 14:52:57 +0200

    added helper function for tracks

--- helper.py
+++ helper.py
@@ -1,7 +1,12 @@
-import numpy as np
+import sys
+from io import StringIO
 from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
 from astropy.io import fits
 from scipy.spatial.transform import Rotation as R
+from scipy.interpolate import make_interp_spline
 
 output_dir = (Path(__file__).parent.resolve() / 'output').absolute()
 output_dir.mkdir(exist_ok=True)
@@ -143,9 +148,179 @@ def apply_araa_style(ax):
         how many digits to show on the ticks, by default 0
     """
     from matplotlib.ticker import StrMethodFormatter
-    ax.xaxis.set_major_formatter(StrMethodFormatter(f'{{x:,g}}'))
-    ax.yaxis.set_major_formatter(StrMethodFormatter(f'{{x:,g}}'))
+    ax.xaxis.set_major_formatter(StrMethodFormatter('{x:,g}'))
+    ax.yaxis.set_major_formatter(StrMethodFormatter('{x:,g}'))
 
     for axis in [ax.xaxis, ax.yaxis]:
         axis.label.set_fontweight('bold')
         axis.label.set_fontstyle('italic')
+
+
+class Capturing(list):
+    """Context manager capturing standard output of whatever is called in it.
+
+    Keywords
+    --------
+    stderr : bool
+        if True will capture the standard error instead of standard output.
+        defaulats to False
+
+    Examples
+    --------
+    >>> with Capturing() as output:
+    >>>     do_something(my_object)
+
+    `output` is now a list containing the lines printed by the function call.
+
+    This can also be concatenated
+
+    >>> with Capturing() as output:
+    >>>    print('hello world')
+    >>> print('displays on screen')
+    displays on screen
+
+    >>> with output:
+    >>>     print('hello world2')
+    >>> print('output:', output)
+    output: ['hello world', 'hello world2']
+
+    >>> import warnings
+    >>> with output, Capturing(stderr=True) as err:
+    >>>     print('hello world2')
+    >>>     warnings.warn('testwarning')
+    >>> print(output)
+    output: ['hello world', 'hello world2']
+
+    >>> print('error:', err[0].split(':')[-1])
+    error:  testwarning
+
+    Mostly copied from [this stackoverflow answer](http://stackoverflow.com/a/16571630/2108771)
+
+    """
+
+    def __init__(self, /, *args, stderr=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._error = stderr
+
+    def __enter__(self):
+        """Start capturing output when entering the context"""
+        if self._error:
+            self._std = sys.stderr
+            sys.stderr = self._stringio = StringIO()
+        else:
+            self._std = sys.stdout
+            sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        """Get & return the collected output when exiting context"""
+        self.extend(self._stringio.getvalue().splitlines())
+        if self._error:
+            sys.stderr = self._std
+        else:
+            sys.stdout = self._std
+
+
+def plot_time_path(x, y, time, snaps=[], label=None, spline=True, xlog=True, tlog=False, eps=0.05, ax=None, scatter_kw=None, **kwargs):
+    """plot an evolutionary track along 2D and marks points in time
+
+    Parameters
+    ----------
+    x : array
+        x-track
+    y : array
+        y-track
+    time : time
+        time axis along x and y
+
+    snaps : list, optional
+        the snapshots at which to mark points along th path, by default []
+    label : str, optional
+        label for that track, by default None
+    spline : bool, optional
+        if true, then spline-interpolate the given points to smooth the track, by default True
+    xlog : bool
+        if x-axis is logarithmic, by default True
+    tlog : bool, optional
+        if time interpolation is done on a log or linear resolution, by default False
+    eps : float, optional
+        by how much to extrapolate the arrow forward and back in time
+        - change sign to flip the arror
+        - set to zero for no arrow
+        - give a 2-element array for different fractional parts backward/forward
+    ax : matplotlib.Axes
+        axes into which to plot, by default None which will create axes
+    scatter_kw : dict
+        keywords for the scatter plot making the circles
+
+    Other Keywords are passed to the `ax.plot` command.
+
+    Example:
+    >>> x = np.geomspace(0.1, 2, 15)
+    >>> y = np.sin(x)
+    >>> t = x**2
+    >>> f, ax = plt.subplots()
+    >>> helper.plot_time_path(x, y, t, snaps=[0.1, 1, 2, 3], spline=False, ax=ax)
+    >>> helper.plot_time_path(x, y, t, snaps=[0.1, 1, 2, 3], tlog=True, spline=True, ax=ax)
+    """
+    if ax is None:
+        _, ax = plt.subplots()
+
+    if xlog:
+        ax.set_xscale('log')
+
+    # create the splines
+    k = 0 + 3 * spline
+    spl_x = make_interp_spline(time, x, k=k)
+    spl_y = make_interp_spline(time, y, k=k)
+
+    # get a smoother interpolation between the points
+    if spline:
+        if tlog:
+            time_smooth = np.geomspace(time[0], time[-1], 50)
+        else:
+            time_smooth = np.linspace(time[0], time[-1], 50)
+
+        x_smooth = spl_x(time_smooth)
+        y_smooth = spl_y(time_smooth)
+    else:
+        time_smooth = time
+        x_smooth = x
+        y_smooth = y
+
+    line, = ax.plot(x_smooth, y_smooth, label=label, **kwargs)
+
+    eps = eps * np.ones(2)
+    if not np.all(eps == 0.0):
+        # add an arrow head slightly extrapolated
+
+        _x0 = spl_x(time[-1] * (1 - eps[0]))
+        _y0 = spl_y(time[-1] * (1 - eps[0]))
+        _x1 = spl_x(time[-1] * (1 + eps[1]))
+        _y1 = spl_y(time[-1] * (1 + eps[1]))
+
+        # opt = dict(color=line.get_color(), arrowstyle=f'simple,head_width=.75,head_length=.75,lw={line.get_linewidth()}', connectionstyle='arc3,rad=0')
+        lw = line.get_linewidth()
+        opt = dict(color=line.get_color(),
+                   linestyle=line.get_linestyle(),
+                   alpha=line.get_alpha(),
+                   linewidth=lw,
+                   arrowstyle=f'-|>,head_width={0.3*lw},head_length={0.6*lw}')
+        ax.annotate('', xy=(_x1, _y1), xycoords='data', xytext=(_x0, _y0), textcoords='data', arrowprops=opt, size=5, annotation_clip=False)
+
+    # add the time circles
+    if len(snaps) > 0:
+
+        for it, _t in enumerate(snaps):
+            _x = spl_x(_t)
+            _y = spl_y(_t)
+
+            # set properties of the circles
+            if scatter_kw is None:
+                scatter_kw = {}
+            scatter_kw['s'] = scatter_kw.get('s', 100)
+            scatter_kw['fc'] = scatter_kw.get('fc', line.get_color())
+            scatter_kw['zorder'] = scatter_kw.get('zorder', 110)
+
+            sca = ax.scatter(_x, _y, **scatter_kw)
+            ax.text(_x, _y, f'{_t:.1g}', horizontalalignment='center', verticalalignment='center', fontsize='xx-small', color='w', zorder=sca.zorder + 1)
